ba94b3d594d7479ada53541c9534f28c6cd4376b
EDITH5607 <aadharshajith5607@gmail.com>
2026-01-26 00:46:04 +0530

    centralized error handling added to the existing error handles

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -10,7 +10,7 @@ import (
 
 func (app *application)home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w,r)
+		app.NotFound(w)
 		return 
 	}
 	
@@ -22,22 +22,21 @@ func (app *application)home(w http.ResponseWriter, r *http.Request) {
 	}
 	ts,err := template.ParseFiles(files...)
 	if err != nil {
-		app.ErrLog.Println(err.Error())
-		http.Error(w,"Internal Server Error!!", 500)
+		app.serverError(w, err)
 		return
 	}
 
 	err = ts.ExecuteTemplate(w,"base", nil)
 	if err!=nil {
 		app.ErrLog.Println(err.Error())
-		http.Error(w, "Internal Server Error!!",500)
-	} 
+		app.serverError(w, err)
+		} 
 }
 
 func (app *application)snippetView(w http.ResponseWriter, r *http.Request) {
 	id,err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err!= nil || id < 1 {
-		http.NotFound(w,r)
+		app.NotFound(w)
 		return
 	}
 	fmt.Fprintf(w,"Display a specific snippet with ID : %d", id)
@@ -47,7 +46,7 @@ func (app *application)snippetView(w http.ResponseWriter, r *http.Request) {
 func (app *application)snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost) // this way we can send tokens for verification.
-		http.Error(w, "post is not allowed ", http.StatusMethodNotAllowed)
+		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
 	w.Write([]byte("Create a new Snippet !!"))
