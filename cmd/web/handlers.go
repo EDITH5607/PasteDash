39ff37647e9009926cf6b6fb2e94ed74393b0a35
EDITH5607 <aadharshajith5607@gmail.com>
2026-01-25 23:59:32 +0530

    inject the dependecies

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,13 +3,12 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
 )
 
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application)home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w,r)
 		return 
@@ -23,19 +22,19 @@ func home(w http.ResponseWriter, r *http.Request) {
 	}
 	ts,err := template.ParseFiles(files...)
 	if err != nil {
-		log.Println(err.Error())
+		app.ErrLog.Println(err.Error())
 		http.Error(w,"Internal Server Error!!", 500)
 		return
 	}
 
 	err = ts.ExecuteTemplate(w,"base", nil)
 	if err!=nil {
-		log.Println(err.Error())
+		app.ErrLog.Println(err.Error())
 		http.Error(w, "Internal Server Error!!",500)
 	} 
 }
 
-func snippetView(w http.ResponseWriter, r *http.Request) {
+func (app *application)snippetView(w http.ResponseWriter, r *http.Request) {
 	id,err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err!= nil || id < 1 {
 		http.NotFound(w,r)
@@ -45,7 +44,7 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
+func (app *application)snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost) // this way we can send tokens for verification.
 		http.Error(w, "post is not allowed ", http.StatusMethodNotAllowed)
