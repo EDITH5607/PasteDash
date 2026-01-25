8482497fa8570454090513e2f818cab0325e5b97
EDITH5607 <aadharshajith5607@gmail.com>
2026-01-25 11:50:54 +0530

    static files and its handlers, cli flag for addr are added

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,22 +1,30 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	//getting http network address through Cli
+	addr := flag.String("addr",":4000","HTTP Network Address")
+	flag.Parse()
 	// router
 	mux := http.NewServeMux()
+
+	//file server
+	fs:= http.FileServer(http.Dir("./ui/static"))
 
 	//routes 
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.Handle("/static/",http.StripPrefix("/static",fs))
 
 	//server initialization and starting
-	log.Println("Starting Server on :127.0.0.1:4000")
-	err := http.ListenAndServe("127.0.0.1:4000", mux)
+	log.Printf("Starting Server on :127.0.0.1:%s",*addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 
 }
