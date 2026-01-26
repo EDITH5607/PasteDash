9aaf7075cb0c4c2d9c1bf94d78e796ce7496b5fb
EDITH5607 <aadharshajith5607@gmail.com>
2026-01-26 23:27:51 +0530

    dependency injected using application struct, include db instance in application

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,19 +7,19 @@ import (
 	"log"
 	"net/http"
 	"os"
-
+	"github.com/EDITH5607/PasteDash/internal/models"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct{
 	ErrLog *log.Logger
 	InfoLog *log.Logger
+	Snippet *models.SnippetModel
 }
 
 
 func main() {
 	//getting http network address through Cli
-
 	addr := flag.String("addr",":4000","HTTP Network Address")
 	dsn := flag.String("dsn", "web:helloworld@/pastedash?parseTime=true", "MySQL data source name!!!")
 	flag.Parse()
@@ -44,22 +44,8 @@ func main() {
 
 
 	//InfoLog and ErrorLog
-
 	InfoLog := log.New(os.Stdout,"INFO\t", log.Ldate | log.Ltime)
 	ErrLog := log.New(os.Stderr, "ERROR\t", log.Ldate | log.Ltime | log.Lshortfile)
-	// initialzing an struct of custom logging
-	app := &application{
-		ErrLog: ErrLog,
-		InfoLog: InfoLog,
-	} 
-
-	// initializing server with custom error logging.
-
-	srv := &http.Server{
-		Addr: *addr,
-		ErrorLog: ErrLog,
-		Handler: app.routes(),
-	}
 
 
 	//db connection
@@ -70,8 +56,21 @@ func main() {
 	defer db.Close()
 
 
-	//server initialization and starting
+	// initialzing an struct of custom logging
+	app := &application{
+		ErrLog: ErrLog,
+		InfoLog: InfoLog,
+		Snippet: &models.SnippetModel{DB:db},
+	} 
 
+	// initializing server with custom error logging.
+	srv := &http.Server{
+		Addr: *addr,
+		ErrorLog: ErrLog,
+		Handler: app.routes(),
+	}
+
+	//server initialization and starting
 	InfoLog.Printf("Starting Server on :127.0.0.1:%s",*addr)
 	err = srv.ListenAndServe()
 	ErrLog.Fatal(err)
