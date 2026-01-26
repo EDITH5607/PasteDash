142401f1e8a5ace7ee069cfdcbb22972c2749dff
EDITH5607 <aadharshajith5607@gmail.com>
2026-01-26 21:36:01 +0530

    database model and methods added

---
+++ internal/models/snippets.go
@@ -0,0 +1,31 @@
+package models
+
+import (
+	"database/sql"
+	"time"
+)
+
+
+type Snippet struct {
+	ID int
+	Title string
+	Content string
+	Create time.Time
+	Expire time.Time
+}
+
+type SnippetModel struct{
+	db *sql.DB
+}
+
+func (m *SnippetModel) insert(title string, content string, expire int) (int, error){
+	return 0, nil
+}
+
+func (m *SnippetModel) get(id int) (*Snippet, error) {
+	return nil, nil
+}
+
+func (m *SnippetModel) latest() ([]*Snippet, error) {
+	return nil, nil
+}
