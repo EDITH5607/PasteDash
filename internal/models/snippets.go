2258fc3eabecc71d0b14c8c0f7cc8d3d198bbf32
EDITH5607 <aadharshajith5607@gmail.com>
2026-01-27 23:24:31 +0530

    insert function completed

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -19,7 +19,18 @@ type SnippetModel struct{
 }
 
 func (m *SnippetModel) insert(title string, content string, expire int) (int, error){
-	return 0, nil
+	stmt := `INSERT INTO snippets (title, content, created, expires)
+		   VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+	result, err := m.DB.Exec(stmt,title,content,expire)
+	if err!=nil {
+		return 0,err
+	}
+	id,err := result.LastInsertId()
+	if err!=nil{
+		return 0,err
+	}
+
+	return int(id), nil
 }
 
 func (m *SnippetModel) get(id int) (*Snippet, error) {
