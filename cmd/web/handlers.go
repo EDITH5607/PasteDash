ab9e024fd79bc52d483d487628dc64b5f8fad7da
EDITH5607 <aadharshajith5607@gmail.com>
2026-01-30 02:49:27 +0530

    snippetview handler complete with search using id...

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,53 +1,73 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"html/template"
 	"net/http"
 	"strconv"
+
+	"github.com/EDITH5607/PasteDash/internal/models"
 )
 
-
-func (app *application)home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		app.NotFound(w)
-		return 
+		return
 	}
-	
+
 	files := []string{
 		"./ui/html/pages/home.html",
 		"./ui/html/base.html",
 		"./ui/html/partials/nav.html",
-
 	}
-	ts,err := template.ParseFiles(files...)
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
 
-	err = ts.ExecuteTemplate(w,"base", nil)
-	if err!=nil {
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
 		app.ErrLog.Println(err.Error())
 		app.serverError(w, err)
-		} 
+	}
 }
 
-func (app *application)snippetView(w http.ResponseWriter, r *http.Request) {
-	id,err := strconv.Atoi(r.URL.Query().Get("id"))
-	if err!= nil || id < 1 {
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
 		app.NotFound(w)
 		return
 	}
-	fmt.Fprintf(w,"Display a specific snippet with ID : %d", id)
 
+	snippet, err := app.Snippet.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.NotFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+	fmt.Fprintf(w, "%v", snippet)
 }
 
-func (app *application)snippetCreate(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost) // this way we can send tokens for verification.
 		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
+
+	title := "0 snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
+	expires := 7
+	id, err := app.Snippet.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
 	w.Write([]byte("Create a new Snippet !!"))
 }
