1693a35ee0be0ab015b6feee79a800743720121e
EDITH5607 <aadharshajith5607@gmail.com>
2026-01-30 02:50:41 +0530

    get function completed with single query execution which return single row of values,

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"database/sql"
+	"errors"
 	"time"
 )
 
@@ -18,7 +19,7 @@ type SnippetModel struct{
 	DB *sql.DB
 }
 
-func (m *SnippetModel) insert(title string, content string, expire int) (int, error){
+func (m *SnippetModel) Insert(title string, content string, expire int) (int, error){
 	stmt := `INSERT INTO snippets (title, content, created, expires)
 		   VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
 	result, err := m.DB.Exec(stmt,title,content,expire)
@@ -33,10 +34,23 @@ func (m *SnippetModel) insert(title string, content string, expire int) (int, er
 	return int(id), nil
 }
 
-func (m *SnippetModel) get(id int) (*Snippet, error) {
-	return nil, nil
+func (m *SnippetModel) Get(id int) (*Snippet, error) {
+	stmt := `SELECT id, title, content, created, expires FROM snippets
+			WHERE expires > UTC_TIMESTAMP() AND id = ?`
+	row := m.DB.QueryRow(stmt,id)
+	s := &Snippet{}
+	err := row.Scan(&s.ID,&s.Title,&s.Content,&s.Create,&s.Expire)
+	if err !=nil {
+		if errors.Is(err,sql.ErrNoRows) {
+			return nil,ErrNoRecord
+			
+		} else {
+			return nil,err
+		}
+	}
+	return s, nil
 }
 
-func (m *SnippetModel) latest() ([]*Snippet, error) {
+func (m *SnippetModel) Latest() ([]*Snippet, error) {
 	return nil, nil
 }
