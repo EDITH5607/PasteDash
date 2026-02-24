69db5c4a0fda58eafbdccbcf35e74d82e5c1114f
EDITH5607 <aadharshajith5607@gmail.com>
2026-02-24 14:14:26 +0530

    error message for user records added

--- internal/models/errors.go
+++ internal/models/errors.go
@@ -2,4 +2,9 @@ package models
 
 import "errors"
 
-var ErrNoRecord = errors.New("models: no matching Record Found !!")
+var (
+	ErrNoRecord = errors.New("models: no matching Record Found !!")
+	ErrInvalidCredential = errors.New("models: Invalid Credentials !!")
+	ErrDuplicateEmail = errors.New("models: Duplicate Email !!!")
+
+)
