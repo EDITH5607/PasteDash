4adbd1876897f2aa3fc068031a319503cb7b0d8c
EDITH5607 <aadharshajith5607@gmail.com>
2026-02-25 21:32:54 +0530

    authentication function of checking the authentication ID

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -73,3 +73,7 @@ func (app *application) clientError(w http.ResponseWriter, status int) {
 func (app *application) NotFound(w http.ResponseWriter) {
 	app.clientError(w, http.StatusNotFound)
 }
+
+func (app *application) isAuthenticated (r *http.Request) bool {
+	return app.sessionManager.Exists(r.Context(), "AuthenticatedUserID")
+}
