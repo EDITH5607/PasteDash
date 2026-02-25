d4f17f8b2c29a6bd75323a709f3a511b7d28e3d9
EDITH5607 <aadharshajith5607@gmail.com>
2026-02-25 21:31:00 +0530

    Authentication middleware is added

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -3,6 +3,23 @@ package main
 import "net/http"
 
 
+/* 
+	we make outer function to pass the next http.handler{next middleware} because the returning innerfunction cannot accept next as parameter like
+	func(w http.ResponseWriter, r *http.Request, next http.Handler) {
+		app.InfoLog.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
+		 next.ServeHTTP(w,r)
+	}
+	then this will not be a http.Handler type
+	it's actually nesting them. Because each middleware calls next.ServeHTTP(), the computer creates a "stack."
+    -secureHeaders starts...
+        -Inside secureHeaders, logRequest starts...
+            -Inside logRequest, homeHandler starts...
+            -homeHandler finishes.
+        -logRequest finishes.
+    -secureHeaders finishes.
+This is why we say it's sequential, but also wrapped. The outer functions stay "active" while the inner functions are running.
+*/
+
 
 func (app *application)logRequest(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -20,6 +37,16 @@ func securityHeader(next http.Handler) http.Handler {
 		w.Header().Set("X-XSS-Protection", "0")
 		next.ServeHTTP(w,r)
 	}) 	
+}
 
-
+func (app *application) requiredAuthentication(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !app.isAuthenticated(r) {
+			http.Redirect(w,r,"/user/login",http.StatusSeeOther)
+			return
+		}
+		w.Header().Add("Cache-Control","no-store")
+		next.ServeHTTP(w,r)
+	
+	})
 }
