452bd87761baf23f390414a2d8f1182334482278
EDITH5607 <aadharshajith5607@gmail.com>
2026-02-28 17:01:55 +0530

    nosurf middleware implemented

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -1,9 +1,13 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
 
+	"github.com/justinas/nosurf"
+)
 
-/* 
+/*
 	we make outer function to pass the next http.handler{next middleware} because the returning innerfunction cannot accept next as parameter like
 	func(w http.ResponseWriter, r *http.Request, next http.Handler) {
 		app.InfoLog.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
@@ -18,10 +22,14 @@ import "net/http"
         -logRequest finishes.
     -secureHeaders finishes.
 This is why we say it's sequential, but also wrapped. The outer functions stay "active" while the inner functions are running.
+
+
+The outerfn execute when the server is started and return the innerfn . this inner fn will execute when ever the request comes...
 */
 
 
 func (app *application)logRequest(next http.Handler) http.Handler {
+	fmt.Printf("logRequest middleware called\n\n")
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		app.InfoLog.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
 		 next.ServeHTTP(w,r)
@@ -29,6 +37,7 @@ func (app *application)logRequest(next http.Handler) http.Handler {
 }
 
 func securityHeader(next http.Handler) http.Handler {
+	fmt.Printf("security header  middleware called\n\n")
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
 		w.Header().Set("Content-Security-Policy", "default-src 'self'; style src 'self' fonts.googleapis.com; font-src fonts.gstatic.com")
 		w.Header().Set("Referrer-Policy", "origin-when-cross-origin")
@@ -36,10 +45,12 @@ func securityHeader(next http.Handler) http.Handler {
 		w.Header().Set("X-Frame-Options", "deny")
 		w.Header().Set("X-XSS-Protection", "0")
 		next.ServeHTTP(w,r)
+		fmt.Print("called security header after middleware")
 	}) 	
 }
 
 func (app *application) requiredAuthentication(next http.Handler) http.Handler {
+	fmt.Println("authentication middleware called")
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if !app.isAuthenticated(r) {
 			http.Redirect(w,r,"/user/login",http.StatusSeeOther)
@@ -49,4 +60,19 @@ func (app *application) requiredAuthentication(next http.Handler) http.Handler {
 		next.ServeHTTP(w,r)
 	
 	})
+}
+
+
+
+
+// the actual object that returned by the noSurf middleware is the csrfhandler and when we write any code before that is just configering that 
+func noSurf(next http.Handler) http.Handler {
+	fmt.Println("surf middleware called")
+	csrfHandler := nosurf.New(next)
+	csrfHandler.SetBaseCookie(http.Cookie{
+		HttpOnly: true,
+		Path: "/",
+		Secure: true,
+	})
+	return csrfHandler
 }
